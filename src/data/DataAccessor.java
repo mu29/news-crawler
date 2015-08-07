1ffa5ee07d0c2ffe29ea1c72fd87d9f3f3b7a7de
InJung Chung <mu29gl@gmail.com>
2015-08-07 16:45:33 +0900

    Save post

--- src/data/DataAccessor.java
+++ src/data/DataAccessor.java
@@ -1,22 +1,43 @@
 package data;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class DataAccessor {
 
-    private ArrayList<Post> postList;
     private static DataAccessor instance;
 
-    private DataAccessor() {
-        postList = new ArrayList<>();
-    }
+    public void savePost(Post post) {
+        try {
+            File dir = new File("./results");
+            if(!dir.exists())
+                dir.mkdir();
 
-    public void addPost(Post post) {
-        postList.add(post);
-    }
+            FileWriter fw = new FileWriter("./results/" +
+                    (post.getContents().equals("기사 내용을 가져올 수 없습니다.") ? "_" : "") + post.getTitle());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write("[Title]");
+            bw.newLine();
+            bw.write(post.getTitle());
+            bw.newLine();
+            bw.newLine();
+            bw.write("[Link]");
+            bw.newLine();
+            bw.write(post.getLink());
+            bw.newLine();
+            bw.newLine();
+            bw.write("[Contents]");
+            bw.newLine();
+            bw.write(post.getContents());
 
-    public void save() {
-        
+            bw.close();
+
+            System.out.println(post.getTitle() + " 기사를 저장했습니다.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static DataAccessor getInstance() {
