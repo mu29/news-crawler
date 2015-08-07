62ce530ee3cdb76ff8c8fde2e2ed7418d2dfd737
InJung Chung <mu29gl@gmail.com>
2015-08-07 17:55:14 +0900

    Move getContents, isMatch function to Crawler class

--- src/crawler/Crawler.java
+++ src/crawler/Crawler.java
@@ -1,6 +1,11 @@
 package crawler;
 
-import java.util.Date;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.*;
 
 public abstract class Crawler {
 
@@ -21,6 +26,44 @@ public abstract class Crawler {
 
     public void setPage(int page) {
         this.page = page;
+    }
+
+    protected String getContents(String url, String sample) {
+        List<String> contentsList = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Elements elements = doc.select("div");
+            for (Element e : elements) {
+                if (isMatch(sample, e.text()))
+                    contentsList.add(e.text());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Collections.sort(contentsList, new StringLengthListSort());
+
+        return contentsList.size() > 0 ? contentsList.get(0) : "기사 내용을 가져올 수 없습니다.";
+    }
+
+    protected boolean isMatch(String sample, String text) {
+        if (sample.length() * 2 > text.length())
+            return false;
+
+        int matchCount = 0;
+        String[] samples = sample.split(" ");
+        for (String s : samples) {
+            if (text.contains(s))
+                matchCount++;
+        }
+
+        return (matchCount * 100 / samples.length) > 30;
+    }
+
+    class StringLengthListSort implements Comparator<String> {
+        @Override
+        public int compare(String s1, String s2) {
+            return s1.length() - s2.length();
+        }
     }
 
     protected abstract void makeUrl();
--- src/crawler/NaverCrawler.java
+++ src/crawler/NaverCrawler.java
@@ -65,42 +65,4 @@ public class NaverCrawler extends Crawler {
         }
     }
 
-    private String getContents(String url, String sample) {
-        List<String> contentsList = new ArrayList<>();
-        try {
-            Document doc = Jsoup.connect(url).get();
-            Elements elements = doc.select("div");
-            for (Element e : elements) {
-                if (isMatch(sample, e.text()))
-                    contentsList.add(e.text());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        Collections.sort(contentsList, new StringLengthListSort());
-
-        return contentsList.size() > 0 ? contentsList.get(0) : "기사 내용을 가져올 수 없습니다.";
-    }
-
-    private boolean isMatch(String sample, String text) {
-        if (sample.length() * 2 > text.length())
-            return false;
-
-        int matchCount = 0;
-        String[] samples = sample.split(" ");
-        for (String s : samples) {
-            if (text.contains(s))
-                matchCount++;
-        }
-
-        return (matchCount * 100 / samples.length) > 30;
-    }
-
-    class StringLengthListSort implements Comparator<String> {
-        @Override
-        public int compare(String s1, String s2) {
-            return s1.length() - s2.length();
-        }
-    }
-
 }
