e201aeae93d1cc1d27912260d81444a44a436daa
InJung Chung <mu29gl@gmail.com>
2015-08-07 16:46:30 +0900

    loose structure

--- src/crawler/GoogleCrawler.java
+++ src/crawler/GoogleCrawler.java
@@ -1,11 +1,18 @@
 package crawler;
 
+import java.net.URLEncoder;
+
 public class GoogleCrawler extends Crawler {
+
+    private final String KEYWORD = "&q=";
+    private final String START_DATE = "&startDate=";
+    private final String END_DATE = "&endDate=";
 
     private String location;
 
     public GoogleCrawler(String keyWord) {
         this.keyWord = keyWord;
+        this.baseUrl = "https://www.google.co.kr/?#newwindow=1";
     }
 
     public void setLocation(String location) {
@@ -13,7 +20,21 @@ public class GoogleCrawler extends Crawler {
     }
 
     @Override
-    public void makeUrl() {
+    protected void makeUrl() {
+        try {
+            urlString = baseUrl + KEYWORD + URLEncoder.encode(keyWord, "UTF-8") + "&pd=";
+            if (!startDate.equals("") && !endDate.equals("")) {
+                urlString += START_DATE + startDate + END_DATE + endDate;
+            } else {
+                urlString += "1";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
 
     }
 
